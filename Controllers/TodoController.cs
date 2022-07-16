261aed093d497279acabf72ea6781c8f78d87b8c
VinhQuangHoang <hoangvinhquang69@gmail.com>
2022-07-16 14:15:04 +0700

    add pagination, sort and filter

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Todolist.Entities;
 using Todolist.Models;
 using Todolist.Service;
+using System;
 
 namespace Todolist.Controllers
 {
@@ -23,16 +24,56 @@ namespace Todolist.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            var todos = await _context.Todos.AsNoTracking().Select(e => new TodoViewModel
+            var todos = await _context.Todos.AsNoTracking().Select(e => new Todo
             {
                 Id = e.Id,
                 Title = e.Title,
                 Description = e.Description
             }).ToListAsync();
 
-            return View(todos);
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["TitleSortParm"] = String.IsNullOrEmpty(sortOrder) ? "tiltle_name" : "";
+            ViewData["DescriptionSortParm"] = String.IsNullOrEmpty(sortOrder) ? "description_name" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            var title = from s in _context.Todos
+                        select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                title = title.Where(s => s.Title.Contains(searchString)
+                                       || s.Description.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "tiltle_name":
+                    title = title.OrderByDescending(s => s.Title);
+                    break;
+                case "description_name":
+                    title = title.OrderByDescending(s => s.Description);
+                    break;
+                default:
+                    title = title.OrderBy(s => s.Title);
+                    break;
+            }
+
+            //return View(todos);
+            int pageSize = 3;
+            return View(await PaginatedList<Todo>.CreateAsync(title.AsNoTracking(), page ?? 1, pageSize));
+
+            //return View();
+
         }
 
         [HttpGet]
